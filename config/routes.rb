fecfc0c040d6e58907239d7845ec679aeb441a3b
Alonso Falconi <alonso.fl@live.com>
2021-11-24 13:34:17 -0500

    Heroku create

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  devise_for :users
+
+  get 'group/show', to: "groups#show", as: "groups_show"
+  get 'group/:id/show', to: "entities#show", as: "entities_show"
+  get 'group/new', to: "groups#new", as: "groups_new"
+  get 'entity/new', to: "entities#new", as: "entity_new"
+  post "/users/:user_id/group", to: "groups#create", as: "user_create_group"
+  post "/users/:user_id/entity", to: "entities#create", as: "user_create_entity"
+  root to: 'index#index'
+
 end
